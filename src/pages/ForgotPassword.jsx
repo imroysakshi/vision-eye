b9aa2366938d333c2b99410cd4283dc1233f56cf
Sakshi <roysakshi355@gmail.com>
2023-01-13 10:30:48 +0530

    forgot-paasword page added

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  return (
+    <>
+      <div class="row">
+        <h1>Forgot Password</h1>
+        <h6 class="information-text">
+          Enter your registered email to reset your password.
+        </h6>
+        <div class="form-group">
+          <input type="email" name="user_email" id="user_email" />
+          <p>
+            <label for="username">Email</label>
+          </p>
+          <button onclick="showSpinner()">Reset Password</button>
+        </div>
+        <div class="footer">
+          <h5>
+            New here? <a href="#">Sign Up.</a>
+          </h5>
+          <h5>
+            Already have an account? <a href="#">Sign In.</a>
+          </h5>
+          <p class="information-text">
+            <span class="symbols" title="Lots of love from me to YOU!">
+              &hearts;{" "}
+            </span>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ForgotPassword;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,11 +5,7 @@ const SignIn = () => {
     <>
       <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100">
         <h1 className="text-2xl font-bold text-center">Login</h1>
-        <form
-          novalidate=""
-          action=""
-          className="space-y-6 ng-untouched ng-pristine ng-valid"
-        >
+        <form action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
           <div className="space-y-1 text-sm">
             <label for="username" className="block dark:text-gray-400">
               Name
